d7dd40d493a40945c762b3e0d095bc56eab3000a
Sneha <snehabhattacharjee2854@gmail.com>
2026-03-29 16:57:14 +0530

    Fix: Added basename to React Router for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { TimerProvider } from './context/TimerContext';
-import { VideoProvider } from './context/VideoContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <VideoProvider>
-      <TimerProvider>
-        <App />
-      </TimerProvider>
-    </VideoProvider>
+    {/* ADD THE BASENAME RIGHT HERE */}
+    <BrowserRouter basename="/Learnova-Ai/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
