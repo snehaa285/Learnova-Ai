ae9e55f95ce6b307a78400d4c8d2cd5a359d78f4
Sneha <snehabhattacharjee2854@gmail.com>
2026-03-29 17:03:33 +0530

    Fix: Added React Router basename

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // THIS MUST BE EXACTLY YOUR GITHUB REPO NAME
   base: '/Learnova-Ai/', 
   plugins: [
     react(),
