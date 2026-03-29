0b4c050ca6dfe671d5bdcff43d8769bb73d1839c
Sneha <snehabhattacharjee2854@gmail.com>
2026-03-29 17:13:31 +0530

    Fix: Added missing Auth exports

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; // <-- This was missing!
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDdePsV4pXC_kClboJglxols4Z6QXNS4nk",
   authDomain: "learnova-ai-fdcf5.firebaseapp.com",
@@ -16,6 +12,9 @@ const firebaseConfig = {
   measurementId: "G-1Q3TJ6F7FP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// 👇 THESE ARE THE EXPORTS YOUR APP NEEDS TO RUN 👇
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
